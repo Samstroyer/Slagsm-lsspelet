06bd9bbadaa14d5bbc9556434f5ec3ed03be0112
Samuel Palmér <samiskramis@gmail.com>
2021-10-26 14:57:10 +0200

    Final Commit

--- Slagsmålsspelet/Program.cs
+++ Slagsmålsspelet/Program.cs
@@ -17,6 +17,9 @@ namespace Slagsmålsspelet
 
 
             Console.WriteLine("Welcome to the figther simulator where everything is chance based!");
+            Console.WriteLine($"{champion} in this universe as the master is contested by the {contestor}! It is time to battle!");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
 
             while (championHp > 0 && contestorHp > 0)
             {
@@ -28,6 +31,39 @@ namespace Slagsmålsspelet
                 championHp -= contestorAttack;
                 contestorHp -= championAttack;
             }
+
+            Console.WriteLine("These are the Contestors damage rolls:");
+            foreach (int roll in contestorRolls)
+            {
+                Console.WriteLine(roll);
+            }
+            Console.WriteLine("These are the Champions damage rolls:");
+            foreach (int roll in championRolls)
+            {
+                Console.WriteLine(roll);
+            }
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+
+            Console.WriteLine("The fight has ended!");
+            System.Threading.Thread.Sleep(400);
+            Console.WriteLine("And the winner is:");
+            if ((championHp < 0) && (contestorHp < 0))
+            {
+                Console.WriteLine("Actually nobody! It was a draw as both skulls were split on site.");
+            }
+            else if (contestorHp < 0)
+            {
+                Console.WriteLine($"Champion {champion} is the winner, {champion} is actually unkillable!");
+            }
+            else
+            {
+                Console.WriteLine($"The contestor, {contestor}, was able to slaughter the champion, {champion}.\nThis was a sad day really!");
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
